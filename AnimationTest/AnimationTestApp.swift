b33459ef63d45f9bedbd13f623878bd6c274e19f
dezdoesit <dez.blair@icloud.com>
2023-10-11 20:50:23 -0400

    Initial Commit

---
+++ AnimationTest/AnimationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationTestApp.swift
+//  AnimationTest
+//
+//  Created by dmoney on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
